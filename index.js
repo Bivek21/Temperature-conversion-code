7e28c26d5cead36cffd697f817b85ec7bc30eed2
Bivek Baral <baralbibek1234@gmail.com>
2024-08-27 23:51:50 +0930

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//Temperature conversion program
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
+function convert() {
+  if (toFahrenheit.checked) {
+    temp = Number(textBox.value);
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = temp.toFixed(1) + "°F";
+  } else if (toCelsius.checked) {
+    temp = Number(textBox.value);
+    temp = (5 / 9) * (temp - 32);
+    result.textContent = temp.toFixed(1) + "°";
+  } else {
+    result.textContent = "select a unit";
+  }
+}
